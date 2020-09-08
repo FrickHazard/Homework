d7703841e2040ae09160416beeb20c625c212e65
Gretchen Doe <gretchendoe@Gretchens-MBP.home>
2020-09-08 15:40:03 -0400

    Update codewars.lean

--- Theorem Proving In Lean/codewars.lean
+++ Theorem Proving In Lean/codewars.lean
@@ -1,10 +1,12 @@
-open nat
+-- open nat
 
-#check _root_.prime
+-- #check _root_.prime
 
-/-- Fermat conjectured that 2^2^n+1 was always prime-/
-theorem fermat_was_wrong : ¬ (∀ n : ℕ, nat.prime (2^2^n + 1)) :=
-begin
-intro h,
+-- /-- Fermat conjectured that 2^2^n+1 was always prime-/
+-- theorem fermat_was_wrong : ¬ (∀ n : ℕ, nat.prime (2^2^n + 1)) :=
+-- begin
+-- intro h,
 
-end
+-- end
+
+theorem new_mul_left_cancel (a b c : nat) (ha : a ≠ 0) : a * b = a * c → b = c := sorry
